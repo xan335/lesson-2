d188d4a922645182daf8d9352bb9170bcba2a75b
xan335 <hodievhursedhon@gmail.com>
2024-10-07 13:57:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let balance = 10000
+
+let name = prompt('Как вас зовут?');
+
+    if (name = 'Alex') {
+        let account = prompt ('Номер счёта?');
+
+            if (account) {
+                let money = prompt('Сколько обноличить?');
+
+                    if (money <= 10000) {
+                        alert('Снял  ' + money + 
+                             '. Осталось ' + (balance - money))
+                    } else {
+                        alert('Недостаточно средств!')
+                    }
+            } 
+
+
+    } else {
+        alert('Пользователь не найден!')
+    }
+
